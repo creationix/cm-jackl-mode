73c604fb542cf4d9d0ad7fd0254adaedb637c72a
Tim Caswell <tim@creationix.com>
2014-06-26 16:42:49 -0500

    Implement theme changing

--- app.js
+++ app.js
@@ -10,44 +10,36 @@ window.addEventListener("resize", function () {
   windowHeight = window.innerHeight;
 }, true);
 
-var jkl = '-- Inverted question mark!\n(macro (¿ no yes cond)\n  [[:? cond yes no]]\n)\n\n-- Sample output for 3x3 maze\n-- ██████████████\n-- ██      ██  ██\n-- ██  ██████  ██\n-- ██          ██\n-- ██  ██  ██████\n-- ██  ██      ██\n-- ██████████████\n\n(def width 30)\n(def height 30)\n(def size (× width height))\n\n-- Cells point to parent\n(def cells (map (i size) [i null]))\n\n-- Walls flag right and bottom\n(def walls (map (i size) [true true]))\n\n-- Define the sequence of index and right/left\n(def ww (- width 1))\n(def hh (- height 1))\n(def sequence (shuffle (concat\n  (map (i size)\n    (if (< (% i width) ww) [true i])\n  )\n  (map (i size)\n    (if (< (÷ i width) hh) [false i])\n  )\n)))\n\n-- Find the root of a set cell -> cell\n(def (find-root cell)\n  (? (. cell 1) (find-root (. cell 1)) cell)\n)\n\n(for (item sequence)\n  (def i (. item 1))\n  (def root (find-root (. cells i)))\n  (def other (find-root (. cells (+ i (? (. item 0) 1 width)))))\n  (if (≠ (. root 0) (. other 0))\n    (. root 1 other)\n    (. (. walls i) (? (. item 0) 0 1) false)\n  )\n)\n\n(def w (× width 2))\n(def h (× height 2))\n(join "\\n" (map (y (+ h 1))\n  (join "" (map (x (+ w 1))\n    (¿ "  " "██" (or\n      -- Four outer edges are always true\n      (= x 0) (= y 0) (= x w) (= y h)\n      -- Inner cells are more complicated\n      (? (% y 2)\n        (? (% x 2)\n           -- cell middle\n          false\n          -- cell right\n          (. (. walls (+ (÷ (- x 1) 2) (× (÷ y 2) width))) 0)\n        )\n        (? (% x 2)\n          -- cell bottom\n          (. (. walls (+ (÷ x 2) (× (÷ (- y 1) 2) width))) 1)\n          -- cell corner\n          true\n        )\n      )\n    ))\n  ))\n))\n'
-var w = domChanger(AppWindow, document.body);
-w.update([CodeMirrorEditor, jkl, "jackl", "notebook"]);
 
-function CodeMirrorEditor(emit, refresh) {
-  var code, mode, theme;
-  var cm;
-  var el;
+var jkl = '-- Inverted question mark!\n(macro (¿ no yes cond)\n  [[:? cond yes no]]\n)\n\n-- Sample output for 3x3 maze\n-- ██████████████\n-- ██      ██  ██\n-- ██  ██████  ██\n-- ██          ██\n-- ██  ██  ██████\n-- ██  ██      ██\n-- ██████████████\n\n(def width 30)\n(def height 30)\n(def size (× width height))\n\n-- Cells point to parent\n(def cells (map (i size) [i null]))\n\n-- Walls flag right and bottom\n(def walls (map (i size) [true true]))\n\n-- Define the sequence of index and right/left\n(def ww (- width 1))\n(def hh (- height 1))\n(def sequence (shuffle (concat\n  (map (i size)\n    (if (< (% i width) ww) [true i])\n  )\n  (map (i size)\n    (if (< (÷ i width) hh) [false i])\n  )\n)))\n\n-- Find the root of a set cell -> cell\n(def (find-root cell)\n  (? (. cell 1) (find-root (. cell 1)) cell)\n)\n\n(for (item sequence)\n  (def i (. item 1))\n  (def root (find-root (. cells i)))\n  (def other (find-root (. cells (+ i (? (. item 0) 1 width)))))\n  (if (≠ (. root 0) (. other 0))\n    (. root 1 other)\n    (. (. walls i) (? (. item 0) 0 1) false)\n  )\n)\n\n(def w (× width 2))\n(def h (× height 2))\n(join "\\n" (map (y (+ h 1))\n  (join "" (map (x (+ w 1))\n    (¿ "  " "██" (or\n      -- Four outer edges are always true\n      (= x 0) (= y 0) (= x w) (= y h)\n      -- Inner cells are more complicated\n      (? (% y 2)\n        (? (% x 2)\n           -- cell middle\n          false\n          -- cell right\n          (. (. walls (+ (÷ (- x 1) 2) (× (÷ y 2) width))) 0)\n        )\n        (? (% x 2)\n          -- cell bottom\n          (. (. walls (+ (÷ x 2) (× (÷ (- y 1) 2) width))) 1)\n          -- cell corner\n          true\n        )\n      )\n    ))\n  ))\n))\n';
 
-  function setup(elt) {
-    el = elt;
-  }
+var windows = [
+  { title: "bananas/samples/maze.jkl", code: jkl, mode: "jackl" }
+];
+
+var d = domChanger(Desktop, document.body);
+d.update(windows);
+
+function Desktop(emit, refresh) {
+  var isDark = false;
+  window.addEventListener("keydown", onKeyDown);
 
   return { render: render };
 
-  function render(newCode, newMode, newTheme) {
-    if (newMode !== mode || newTheme !== theme) {
-      mode = newMode;
-      theme = newTheme;
-      cm = new CodeMirror(setup, {
-        mode: mode,
-        theme: theme,
-        keyMap: "sublime",
-        lineNumbers: true,
-        rulers: [{ column: 80 }],
-        autoCloseBrackets: true,
-        matchBrackets: true,
-        showCursorWhenSelecting: true,
-        styleActiveLine: true,
-      });
+  function onKeyDown(evt) {
+    if (evt.ctrlKey && !evt.shiftKey && !evt.altKey && !evt.metaKey && evt.keyCode === 66) {
+      evt.preventDefault();
+      isDark = !isDark;
+      refresh();
     }
-    if (newCode !== code) {
-      cm.setValue(newCode);
-    }
-    setTimeout(function () {
-      cm.refresh();
-    }, 0);
-    return el;
+  }
+
+  function render(windows) {
+    return windows.map(function (props) {
+      return [AppWindow, isDark, props.title,
+        [CodeMirrorEditor, isDark, props]
+      ];
+    });
   }
 }
 
@@ -56,21 +48,20 @@ function AppWindow(emit, refresh) {
   var height = (windowHeight / 2) | 0;
   var left = ((windowWidth - width) / 2) | 0;
   var top = ((windowHeight - height) / 2) | 0;
-  var title = "";
   var dragging = false;
   window.addEventListener("mouseup", onMouseUp);
   window.addEventListener("mousemove", onMouseMove);
 
   return { render: render };
 
-  function render(child) {
+  function render(isDark, title, child) {
     var style = {
       width: width + "px",
       height: height + "px",
       left: left + "px",
       top: top + "px",
     };
-    return ["dialog.window", { style: style },
+    return ["dialog.window", { style: style, class: isDark ? "dark" : "light"},
       [".content", child],
       [".resize.n", drag(north)],
       [".resize.ne", drag(northEast)],
@@ -163,5 +154,43 @@ function AppWindow(emit, refresh) {
     top += dy;
     left += dx;
     refresh();
+  }
+}
+
+function CodeMirrorEditor() {
+  var code, mode, theme;
+  var el;
+  var cm = new CodeMirror(function (root) {
+    el = root;
+  }, {
+    keyMap: "sublime",
+    // lineNumbers: true,
+    rulers: [{ column: 80 }],
+    autoCloseBrackets: true,
+    matchBrackets: true,
+    showCursorWhenSelecting: true,
+    styleActiveLine: true,
+  });
+
+  return { render: render };
+
+  function render(isDark, props) {
+    var newTheme = isDark ? "notebook-dark" : "notebook";
+    if (newTheme !== theme) {
+      theme = newTheme;
+      cm.setOption("theme", theme);
+    }
+    if (props.mode !== mode) {
+      mode = props.mode;
+      cm.setOption("mode", mode);
+    }
+    if (props.code !== code) {
+      code = props.code;
+      cm.setValue(code);
+    }
+    setTimeout(function () {
+      cm.refresh();
+    }, 0);
+    return el;
   }
 }
