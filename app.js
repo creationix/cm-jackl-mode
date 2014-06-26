8e7040694979236c4c2738f239ca5c357f464edb
Tim Caswell <tim@creationix.com>
2014-06-26 17:23:49 -0500

    Fix constraints

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 /*global CodeMirror domChanger*/
 "use strict";
 
-var windowWidth = window.innerWidth;
-window.addEventListener("resize", function () {
-  windowWidth = window.innerWidth;
-}, true);
-var windowHeight = window.innerHeight;
-window.addEventListener("resize", function () {
-  windowHeight = window.innerHeight;
-}, true);
-
-
 var jkl = '-- Inverted question mark!\n(macro (¿ no yes cond)\n  [[:? cond yes no]]\n)\n\n-- Sample output for 3x3 maze\n-- ██████████████\n-- ██      ██  ██\n-- ██  ██████  ██\n-- ██          ██\n-- ██  ██  ██████\n-- ██  ██      ██\n-- ██████████████\n\n(def width 30)\n(def height 30)\n(def size (× width height))\n\n-- Cells point to parent\n(def cells (map (i size) [i null]))\n\n-- Walls flag right and bottom\n(def walls (map (i size) [true true]))\n\n-- Define the sequence of index and right/left\n(def ww (- width 1))\n(def hh (- height 1))\n(def sequence (shuffle (concat\n  (map (i size)\n    (if (< (% i width) ww) [true i])\n  )\n  (map (i size)\n    (if (< (÷ i width) hh) [false i])\n  )\n)))\n\n-- Find the root of a set cell -> cell\n(def (find-root cell)\n  (? (. cell 1) (find-root (. cell 1)) cell)\n)\n\n(for (item sequence)\n  (def i (. item 1))\n  (def root (find-root (. cells i)))\n  (def other (find-root (. cells (+ i (? (. item 0) 1 width)))))\n  (if (≠ (. root 0) (. other 0))\n    (. root 1 other)\n    (. (. walls i) (? (. item 0) 0 1) false)\n  )\n)\n\n(def w (× width 2))\n(def h (× height 2))\n(join "\\n" (map (y (+ h 1))\n  (join "" (map (x (+ w 1))\n    (¿ "  " "██" (or\n      -- Four outer edges are always true\n      (= x 0) (= y 0) (= x w) (= y h)\n      -- Inner cells are more complicated\n      (? (% y 2)\n        (? (% x 2)\n           -- cell middle\n          false\n          -- cell right\n          (. (. walls (+ (÷ (- x 1) 2) (× (÷ y 2) width))) 0)\n        )\n        (? (% x 2)\n          -- cell bottom\n          (. (. walls (+ (÷ x 2) (× (÷ (- y 1) 2) width))) 1)\n          -- cell corner\n          true\n        )\n      )\n    ))\n  ))\n))\n';
 
 var windows = [
@@ -23,8 +13,19 @@ d.update(windows);
 function Desktop(emit, refresh) {
   var isDark = false;
   window.addEventListener("keydown", onKeyDown);
+  window.addEventListener("resize", onResize);
+  var width = window.innerWidth;
+  var height = window.innerHeight;
 
   return { render: render };
+
+  function onResize(evt) {
+    if (window.innerWidth !== width || window.innerHeight !== height) {
+      width = window.innerWidth;
+      height = window.innerHeight;
+      refresh();
+    }
+  }
 
   function onKeyDown(evt) {
     if (evt.ctrlKey && !evt.shiftKey && !evt.altKey && !evt.metaKey && evt.keyCode === 66) {
@@ -36,7 +37,7 @@ function Desktop(emit, refresh) {
 
   function render(windows) {
     return windows.map(function (props) {
-      return [AppWindow, isDark, props.title,
+      return [AppWindow, width, height, isDark, props.title,
         [CodeMirrorEditor, isDark, props]
       ];
     });
@@ -44,17 +45,47 @@ function Desktop(emit, refresh) {
 }
 
 function AppWindow(emit, refresh) {
-  var width = (windowWidth / 2) | 0;
-  var height = (windowHeight / 2) | 0;
-  var left = ((windowWidth - width) / 2) | 0;
-  var top = ((windowHeight - height) / 2) | 0;
+  var width, height, left, top;
   var dragging = false;
   window.addEventListener("mouseup", onMouseUp);
   window.addEventListener("mousemove", onMouseMove);
 
   return { render: render };
 
-  function render(isDark, title, child) {
+  function render(windowWidth, windowHeight, isDark, title, child) {
+
+    if (width === undefined) {
+      width = (windowWidth / 2) | 0;
+      height = (windowHeight / 2) | 0;
+      left = ((windowWidth - width) / 2) | 0;
+      top = ((windowHeight - height) / 2) | 0;
+    }
+
+    // Manually run constraints that edges must be inside desktop and
+    // window must be at least 200x100
+    var right = left + width;
+    var bottom = top + height;
+    if (left < 0) left = 0;
+    if (top < 0) top = 0;
+    if (right > windowWidth) right = windowWidth;
+    if (bottom > windowHeight) bottom = windowHeight;
+    var mid = ((left + right) / 2) | 0;
+    if (mid < ((windowWidth / 2) | 0)) {
+      if (right < left + 200) right = left + 200;
+    }
+    else {
+      if (left > right - 200) left = right - 200;
+    }
+    mid = ((top + bottom) / 2) | 0;
+    if (mid < ((windowHeight / 2) | 0)) {
+      if (bottom < top + 100) bottom = top + 100;
+    }
+    else {
+      if (top > bottom - 100) top = bottom - 100;
+    }
+    width = right - left;
+    height = bottom - top;
+
     var style = {
       width: width + "px",
       height: height + "px",
